838203d5ef4f356ae3d465a467763d7877a5f047
DenisKor3101 <91784316+DenisKor3101@users.noreply.github.com>
2021-10-27 21:23:55 +0300

    Update ezam_bilet5-c.c

--- ezam_bilet5/ezam_bilet5-c.c
+++ ezam_bilet5/ezam_bilet5-c.c
@@ -2,7 +2,7 @@
 
 	int a1, b1;
 	int a2, b2;
-    int a3, b3;
+    	int a3, b3;
 	
 void input1()
 {
@@ -31,22 +31,22 @@ void add()
 
 void subtract()
 {
-    a3 = a1 * b2 - a2 * b1;
-  	b3 = b1 * b2;
-  	printf("subtraction: %d / %d\n", a3, b3);
+    	a3 = a1 * b2 - a2 * b1;
+	b3 = b1 * b2;
+	printf("subtraction: %d / %d\n", a3, b3);
 }
 
 void division()
 {
-    a3 = a1 * b2;
-  	b3 = b1 * a2;
-	  printf("division: %d / %d\n", a3, b3);
+    	a3 = a1 * b2;
+	b3 = b1 * a2;
+	printf("division: %d / %d\n", a3, b3);
 }
 
 void multiplication()
 {
-    a3 = a1 * a2;
-    b3 = b1 * b2;
+    	a3 = a1 * a2;
+    	b3 = b1 * b2;
     
 	printf("multiplication: %d / %d\n", a3, b3);
 }
@@ -62,16 +62,16 @@ int choice = -1;
 
 do
  {
-	    printf("\n");
-    	printf("0 - exit\n");
-      printf("1 - input 1\n");
-  	  printf("2 - input 2\n");
-     	printf("3 - printf\n");
-    	printf("4 - add\n");
-	    printf("5 - subtract\n");
-      printf("6 - devision\n");
-      printf("7 - multiplication\n");
-       printf("8 - translationDecimal\n");
+	printf("\n");
+	printf("0 - exit\n");
+	printf("1 - input 1\n");
+	printf("2 - input 2\n");
+	printf("3 - printf\n");
+	printf("4 - add\n");
+	printf("5 - subtract\n");
+   	printf("6 - devision\n");
+    	printf("7 - multiplication\n");
+    	printf("8 - translationDecimal\n");
 
 	printf("\n");
 	
@@ -82,22 +82,28 @@ do
 	
 	switch(choice)
 	{
-	    	case 1: input1(); break;
-		    case 2: input2(); break;
-		    case 3: print(); break;
-		    case 4: add(); break;
-		    case 5: subtract(); break;
-        case 6: division();break;
-        case 7: multiplication();break;
-        case 8: translationDecimal();break;
+		case 0: return 0; break;
+		case 1: input1(); break;
+		case 2: input2(); break;
+		case 3: print(); break;
+		case 4: add(); break;
+		case 5: subtract(); break;
+        	case 6: division();break;
+       	 	case 7: multiplication();break;
+       	 	case 8: translationDecimal();break;
 	}
-    printf("\n");
-  	printf("press");
-    printf("\n");
+    	printf("\n");
+	printf("press");
+    	printf("\n");
  }
  while(choice != 0);
 	
 	
 	
 	return 0;
+
+	switch(choice)
+	{
+		case 0: return 0; break;
+	}
 }
