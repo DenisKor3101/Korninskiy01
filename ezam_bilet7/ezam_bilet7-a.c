3df27848cdcdb06091b23b7a3595a590f3ee6998
DenisKor3101 <91784316+DenisKor3101@users.noreply.github.com>
2021-10-29 00:55:15 +0300

    Update ezam_bilet7-a.c

--- ezam_bilet7/ezam_bilet7-a.c
+++ ezam_bilet7/ezam_bilet7-a.c
@@ -4,22 +4,16 @@
 #define n_max 100
 
 int n;
-int mmax, mmin;
 int mark[n_max];
 
-//#define mmin 2
-//#define mmax 5
+#define mmin 2
+#define mmax 5
 
 #define mpass 4.6f
 
 int main()
 {
-    printf("Введите минимальный балл: ");
-    scanf("%d", &mmin);
-    //if(mmin < 2) printf("Ошибка: Минимальный балл 2\n"); 
-    printf("Введите максимальный балл: ");
-    scanf("%d", &mmax);
-   // if(mmax > 5) printf("Ошибка: Максимальный балл 5\n"); 
+   
     printf("Введите колличество оценок: ");
     scanf("%d", &n);
     printf("Введите оценки: ");
