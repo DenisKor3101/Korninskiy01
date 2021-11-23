9d0830a78ed2f10e3e290a9601948d31143e171b
DenisKor3101 <91784316+DenisKor3101@users.noreply.github.com>
2021-11-23 21:14:38 +0300

    Update ezam_bilet22-b.c

--- ezam_bilet22/ezam_bilet22-b.c
+++ ezam_bilet22/ezam_bilet22-b.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <string.h> 
+#include <string.h> //strcpy()
+#include <stdlib.h>
 
 struct theRow
 {
@@ -12,106 +13,47 @@ struct theRow
 #define rowNum 3
 struct theRow row[rowNum];
 
-
-void theUpdateRow();
-
-void writeInTxt()
-{
-  
-	FILE *theWriteFile = fopen("exam22.txt", "w+");
-	
-	for(int i = 0; i < rowNum; i++)
-	fprintf(theWriteFile, "%s\t%s\t%d\t%d\n", row[i].title, row[i].mfg, row[i].year, row[i].price);
-	fclose(theWriteFile);
-}
-
 void outputTable()
 {
-	//OUTPUT TABLE
+
 	for(int i = 0; i < rowNum; i++)
 	printf("%s\t%s\t%d\t%d\n", row[i].title, row[i].mfg, row[i].year, row[i].price);
 }
 
 
-void menuCall()
-{
-	
-	int theChooseOne = 0;
-	printf(" [0]  	[1]\n");
-	printf("[EXIT] [UPDATE]\n");
-	printf("Choose(0/1): ");
-	scanf("%d", &theChooseOne);
-	if (theChooseOne == 0)
-	exit(0);
-	else if (theChooseOne == 1)
-	theUpdateRow();
-	else
-	{
-		printf("write 0 or 1\n");
-		menuCall();
-	}
-}
-
-void theUpdateRow()
-{
-    outputTable();
-	int fondRowNum = 0;
-	char xTitle[50];
-	printf("Title: ");
-	scanf("%s", xTitle);
-	printf("Foundindg for %s\n", xTitle);
-	
-
-	for(int i = 0; i < rowNum; i++)
-	{
-		if(strcmp(row[i].title, xTitle) == 0)
-		{
-			fondRowNum++;
-
-			char newTitle[50];
-			printf("New title: ");
-			scanf("%s", newTitle);
-			strcpy(row[i].title, newTitle);
-
-			char newMfg[100];
-			printf("New mfg: ");
-			scanf("%s", newMfg);
-			strcpy(row[i].mfg, newMfg);
-
-			short newYear;
-			printf("New year: ");
-			scanf("%hd", &newYear);
-			row[i].year = newYear;
-
-			int newPrice;
-			printf("New price: ");
-			scanf("%d", &newPrice);
-			row[i].price = newPrice;
-
-		
-			FILE *theUpdateFile = fopen("exam22.bin", "wb+");
-			fwrite(row, sizeof(struct theRow), 3, theUpdateFile);
-			fclose(theUpdateFile);
-			writeInTxt();
-
-			outputTable();
-		}
-		else if(fondRowNum != 0)
-		{
-			printf("Number of founds rows: %d\n", fondRowNum);
-			printf("End finding title %s\n", xTitle);
-			menuCall();
-		}
-	}
-}
 
 int main()
 {
+
+	strcpy(row[0].title, "Video Adapter");
+	strcpy(row[0].mfg, "Nvidia");
+	row[0].year = 2020;
+	row[0].price = 40000;
+	strcpy(row[1].title, "Hard Drive");
+	strcpy(row[1].mfg, "Western Digital");
+	row[1].year = 2019;
+	row[1].price = 5000;
+	strcpy(row[2].title, "Sound Card");
+	strcpy(row[2].mfg, "Realtek");
+	row[2].year = 2015;
+	row[2].price = 1000;
+
+	FILE *theWriteBinFile = fopen("exam22.bin", "wb+");
+	fwrite(row, sizeof(struct theRow), 3, theWriteBinFile);
+	fclose(theWriteBinFile);
+
+
+
 	FILE *theReadFile = fopen("exam22.bin", "rb+");
 	fread(row, sizeof(struct theRow), 3, theReadFile);
 	fclose(theReadFile);
 
-	writeInTxt(); //na 4
+
+	FILE *theWriteFile = fopen("exam22.txt", "w+");
+
+	for(int i = 0; i < rowNum; i++)
+	fprintf(theWriteFile, "%s\t%s\t%d\t%d\n", row[i].title, row[i].mfg, row[i].year, row[i].price);
+	fclose(theWriteFile);
 
 	outputTable();
 
@@ -119,13 +61,13 @@ int main()
 	char xTitle[50];
 	printf("find: ");
 	fgets(xTitle, 50, stdin);
-	
+
 	char *p = strchr(xTitle, '\n');
 	if (p != NULL) *p = 0;
 	p = strchr(xTitle, '\r');
 	if (p != NULL) *p = 0;
 
-	
+
 	int thereIsNoXTitle = 1;
 	for(int i = 0; i < rowNum; i++)
 	if (strcmp(row[i].title, xTitle) == 0)
@@ -136,8 +78,7 @@ int main()
 	if(thereIsNoXTitle == 1)
 	printf("No title found\n");
 
-
-
+	
 
 	return 0;
 }
