f82c92af24bc56676b5eaab0b24e746cd597f668
deniskor <deniskor3101@gmail.com>
2022-09-14 17:36:47 +0300

    Коммит

--- OOOPovarenok/Program.cs
+++ OOOPovarenok/Program.cs
@@ -16,7 +16,7 @@ namespace OOOPovarenok
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormPattern());
         }
     }
 }
